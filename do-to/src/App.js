4738524aa6b96e8ab37a24ad53c39c1f850bd6a0
Madness-Of-Madhan <donmathan264@gmail.com>
2024-08-21 21:15:49 +0530

    i'm done

---
+++ do-to/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './assets/Todo.css';
+import TodoWrapper from './components/TodoWrapper';
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper></TodoWrapper>
+    </div>
+  );
+}
+
+export default App;
